d37810e44001044b7f38ffcd4ea7dfa65f0df0b9
Chifez <Chifez1@gmail.com>
2023-03-01 21:06:55 +0100

    feat:express Api FOR STRIPE working

--- index.ts
+++ index.ts
@@ -9,24 +9,49 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-
 dotenv.config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 interface RequestBody {
 	amount: number;
 	id: number;
 }
+app.get("/config", (req: Request, res: Response) => {
+	res.send({
+		publishablekey: process.env.STRIPE_PUBLIC_KEY,
+	});
+});
+
 app.post("/payment", async (req: Request, res: Response) => {
-	const { amount, id }: RequestBody = req.body;
+	// const { amount, id }: RequestBody = req.body;
 	try {
-		const payment = await stripe.PaymentIntents.create({
-			amount,
-			currency: "usd",
+		const payment = await stripe.paymentIntents.create({
+			amount: 4099,
+			currency: "USD",
 			description: "Faloni",
+			// payment_method: id,
+			// confirm: true,
+			automatic_payment_methods: {
+				enabled: true,
+			},
 		});
-	} catch (error) {}
+		res.send({
+			clientSecret: payment.client_secret,
+			message: "backend dey work oo",
+			success: true,
+		});
+
+		// res.json({
+
+		// });
+	} catch (error) {
+		console.log("Error", error);
+		res.json({
+			message: "payment failed",
+			success: false,
+		});
+	}
 });
 app.listen(process.env.PORT || 400, () => {
-	console.log("port is listening");
+	console.log("port is listening on 400");
 });
