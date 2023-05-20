32ef65e71be264d372c8ae68169988535bce4121
emmanuel <chifez1@gmail.com>
2023-05-20 18:15:39 +0100

    feat:added the verceljson again

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,42 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
@@ -23,39 +49,40 @@ app.use((0, cors_1.default)());
 dotenv_1.default.config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 app.get("/config", (req, res) => {
-    res.send({
-        publishablekey: process.env.STRIPE_PUBLIC_KEY,
-    });
+  res.send({
+    publishablekey: process.env.STRIPE_PUBLIC_KEY,
+  });
 });
-app.post("/payment", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.post("/payment", (req, res) =>
+  __awaiter(void 0, void 0, void 0, function* () {
     // const { amount, id }: RequestBody = req.body;
     try {
-        const payment = yield stripe.paymentIntents.create({
-            amount: 4099,
-            currency: "USD",
-            description: "Faloni",
-            // payment_method: id,
-            // confirm: true,
-            automatic_payment_methods: {
-                enabled: true,
-            },
-        });
-        res.send({
-            clientSecret: payment.client_secret,
-            message: "backend dey work oo",
-            success: true,
-        });
-        // res.json({
-        // });
+      const payment = yield stripe.paymentIntents.create({
+        amount: 4099,
+        currency: "USD",
+        description: "Faloni",
+        // payment_method: id,
+        // confirm: true,
+        automatic_payment_methods: {
+          enabled: true,
+        },
+      });
+      res.send({
+        clientSecret: payment.client_secret,
+        message: "backend dey work oo",
+        success: true,
+      });
+      // res.json({
+      // });
+    } catch (error) {
+      console.log("Error", error);
+      res.json({
+        message: "payment failed",
+        success: false,
+      });
     }
-    catch (error) {
-        console.log("Error", error);
-        res.json({
-            message: "payment failed",
-            success: false,
-        });
-    }
-}));
+  })
+);
 app.listen(process.env.PORT || 400, () => {
-    console.log("port is listening on 400");
+  console.log("port is listening on 400");
 });
--- index.ts
+++ index.ts
@@ -13,45 +13,46 @@ dotenv.config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 interface RequestBody {
-	amount: number;
-	id: number;
+  amount: number;
+  id: number;
 }
-app.get("/config", (req: Request, res: Response) => {
-	res.send({
-		publishablekey: process.env.STRIPE_PUBLIC_KEY,
-	});
+app.get("/config", (_req: Request, res: Response) => {
+  res.send("working backend");
+  return res.send({
+    publishablekey: process.env.STRIPE_PUBLIC_KEY,
+  });
 });
 
-app.post("/payment", async (req: Request, res: Response) => {
-	// const { amount, id }: RequestBody = req.body;
-	try {
-		const payment = await stripe.paymentIntents.create({
-			amount: 4099,
-			currency: "USD",
-			description: "Faloni",
-			// payment_method: id,
-			// confirm: true,
-			automatic_payment_methods: {
-				enabled: true,
-			},
-		});
-		res.send({
-			clientSecret: payment.client_secret,
-			message: "backend dey work oo",
-			success: true,
-		});
+app.post("/payment", async (_req: Request, res: Response) => {
+  // const { amount, id }: RequestBody = req.body;
+  try {
+    const payment = await stripe.paymentIntents.create({
+      amount: 4099,
+      currency: "USD",
+      description: "Faloni",
+      // payment_method: id,
+      // confirm: true,
+      automatic_payment_methods: {
+        enabled: true,
+      },
+    });
+    res.send({
+      clientSecret: payment.client_secret,
+      message: "backend dey work oo",
+      success: true,
+    });
 
-		// res.json({
+    // res.json({
 
-		// });
-	} catch (error) {
-		console.log("Error", error);
-		res.json({
-			message: "payment failed",
-			success: false,
-		});
-	}
+    // });
+  } catch (error) {
+    console.log("Error", error);
+    res.json({
+      message: "payment failed",
+      success: false,
+    });
+  }
 });
 app.listen(process.env.PORT || 400, () => {
-	console.log("port is listening on 400");
+  console.log("port is listening on 400");
 });
